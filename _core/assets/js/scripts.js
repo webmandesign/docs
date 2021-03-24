f7f4546f1369d27830ee5c2dbbf1a0295858d07d
WebMan Design, Oliver Juhas <webmandesigneu@gmail.com>
2021-03-24 23:00:19 +0100

    Updating template

--- _core/assets/js/scripts.js
+++ _core/assets/js/scripts.js
@@ -1,58 +1,60 @@
-'use strict';
+window.addEventListener( 'load', function() {
+	'use strict';
 
-var
-	lastScrollTop  = window.scrollY,
-	ticking        = false,
-	entriesTops    = {},
-	menu           = document.getElementById( 'site-menu' );
-
-function getEntriesTops() {
-	var entries = document.getElementsByClassName( 'entry' );
-
-	for ( var i = 0; i < entries.length; i++ ) {
-		entriesTops[ entries[ i ].offsetTop + entries[ i ].offsetHeight ] = entries[ i ].id;
-	}
-}
-getEntriesTops();
-
-window.addEventListener( 'resize', function( e ) {
-	if ( ! ticking ) {
-		window.requestAnimationFrame( function() {
-			getEntriesTops();
-			ticking = false;
-		} );
-		ticking = true;
-	}
-} );
-
-function checkScroll() {
 	var
-		scrolledY = window.scrollY,
-		id        = false;
+		lastScrollTop  = window.scrollY,
+		ticking        = false,
+		entriesTops    = {},
+		menu           = document.getElementById( 'site-menu' );
 
-	for ( var y in entriesTops ) {
-		if ( scrolledY < y ) {
-			id = entriesTops[ y ];
-			break
+	function getEntriesTops() {
+		var entries = document.getElementsByClassName( 'entry' );
+
+		for ( var i = 0; i < entries.length; i++ ) {
+			entriesTops[ entries[ i ].offsetTop + entries[ i ].offsetHeight ] = entries[ i ].id;
 		}
 	}
+	getEntriesTops();
 
-	if ( id ) {
-		var activeMenuItem = menu.querySelector( '.is-active' );
-		if ( activeMenuItem ) {
-			activeMenuItem.classList.remove( 'is-active' );
+	window.addEventListener( 'resize', function( e ) {
+		if ( ! ticking ) {
+			window.requestAnimationFrame( function() {
+				getEntriesTops();
+				ticking = false;
+			} );
+			ticking = true;
 		}
-		menu.querySelector( '[href="#' + id + '"]' ).classList.add( 'is-active' );
-	}
-}
-checkScroll();
+	} );
 
-window.addEventListener( 'scroll', function( e ) {
-	if ( ! ticking ) {
-		window.requestAnimationFrame( function() {
-			checkScroll();
-			ticking = false;
-		} );
-		ticking = true;
+	function checkScroll() {
+		var
+			scrolledY = window.scrollY,
+			id        = false;
+
+		for ( var y in entriesTops ) {
+			if ( scrolledY < y ) {
+				id = entriesTops[ y ];
+				break
+			}
+		}
+
+		if ( id ) {
+			var activeMenuItem = menu.querySelector( '.is-active' );
+			if ( activeMenuItem ) {
+				activeMenuItem.classList.remove( 'is-active' );
+			}
+			menu.querySelector( '[href="#' + id + '"]' ).classList.add( 'is-active' );
+		}
 	}
-} );
+	checkScroll();
+
+	window.addEventListener( 'scroll', function( e ) {
+		if ( ! ticking ) {
+			window.requestAnimationFrame( function() {
+				checkScroll();
+				ticking = false;
+			} );
+			ticking = true;
+		}
+	} );
+} )();
