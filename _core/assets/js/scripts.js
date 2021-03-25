22b1469611efd210f3bcbd553afdbfb4ac4375a0
WebMan Design, Oliver Juhas <webmandesigneu@gmail.com>
2021-03-25 16:26:15 +0100

    Updating Michelle docs

--- _core/assets/js/scripts.js
+++ _core/assets/js/scripts.js
@@ -1,58 +1,66 @@
-'use strict';
+window.addEventListener( 'load', function() {
+	'use strict';
 
-var
-	lastScrollTop  = window.scrollY,
-	ticking        = false,
-	entriesTops    = {},
-	menu           = document.getElementById( 'site-menu' );
-
-function getEntriesTops() {
-	var entries = document.getElementsByClassName( 'entry' );
-
-	for ( var i = 0; i < entries.length; i++ ) {
-		entriesTops[ entries[ i ].offsetTop + entries[ i ].offsetHeight ] = entries[ i ].id;
-	}
-}
-getEntriesTops();
-
-window.addEventListener( 'resize', function( e ) {
-	if ( ! ticking ) {
-		window.requestAnimationFrame( function() {
-			getEntriesTops();
-			ticking = false;
-		} );
-		ticking = true;
-	}
-} );
-
-function checkScroll() {
 	var
-		scrolledY = window.scrollY,
-		id        = false;
+		lastScrollTop  = window.scrollY,
+		ticking        = false,
+		entriesTops    = {},
+		menu           = document.getElementById( 'site-menu' );
 
-	for ( var y in entriesTops ) {
-		if ( scrolledY < y ) {
-			id = entriesTops[ y ];
-			break
+	// First get list of navigable sections with their `.offsetTop` values.
+
+		function getEntriesTops() {
+			var entries = document.querySelectorAll( '.intro, .entry' );
+
+			for ( var i = 0; i < entries.length; i++ ) {
+				entriesTops[ entries[ i ].offsetTop + entries[ i ].offsetHeight ] = entries[ i ].id;
+			}
 		}
-	}
 
-	if ( id ) {
-		var activeMenuItem = menu.querySelector( '.is-active' );
-		if ( activeMenuItem ) {
-			activeMenuItem.classList.remove( 'is-active' );
-		}
-		menu.querySelector( '[href="#' + id + '"]' ).classList.add( 'is-active' );
-	}
-}
-checkScroll();
+		getEntriesTops();
 
-window.addEventListener( 'scroll', function( e ) {
-	if ( ! ticking ) {
-		window.requestAnimationFrame( function() {
-			checkScroll();
-			ticking = false;
+		window.addEventListener( 'resize', function( e ) {
+			if ( ! ticking ) {
+				window.requestAnimationFrame( function() {
+					getEntriesTops();
+					ticking = false;
+				} );
+				ticking = true;
+			}
 		} );
-		ticking = true;
-	}
+
+	// Now we can apply `.is-active` class on navigation links when scrolling.
+
+		function checkScroll() {
+			var
+				scrolledY = window.scrollY,
+				id        = false;
+
+			for ( var y in entriesTops ) {
+				if ( scrolledY < y ) {
+					id = entriesTops[ y ];
+					break
+				}
+			}
+
+			if ( id ) {
+				var activeMenuItem = menu.querySelector( '.is-active' );
+				if ( activeMenuItem ) {
+					activeMenuItem.classList.remove( 'is-active' );
+				}
+				menu.querySelector( '[href="#' + id + '"]' ).classList.add( 'is-active' );
+			}
+		}
+		checkScroll();
+
+		window.addEventListener( 'scroll', function( e ) {
+			if ( ! ticking ) {
+				window.requestAnimationFrame( function() {
+					checkScroll();
+					ticking = false;
+				} );
+				ticking = true;
+			}
+		} );
+
 } );
